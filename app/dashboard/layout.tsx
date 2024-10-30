add9cd2a91236db16c76f05222f4f73d2722f775
amanprajapati00 <aman.prajapati3000@gmail.com>
2024-10-30 23:06:21 +0530

    changed the layout a bit

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,8 @@
+import Projects from "@/components/components/Projects";
 import Sidebar from "@/components/components/Sidebar";
+import Sortbutton from "@/components/components/Sortbutton";
 import Topbar from "@/components/components/Topbar";
+import View from "@/components/components/View";
 import React from "react";
 
 interface DashboardLayoutProps {
@@ -8,12 +11,16 @@ interface DashboardLayoutProps {
 
 export default function layout({ children }: DashboardLayoutProps) {
   return (
-    <div className="w-screen h-screen">
+    <div className="w-full h-full">
       <div className="w-full h-full flex flex-col pt-3 px-3">
         <Topbar />
-        <div className="w-full h-full flex flex-row">
-          <Sidebar />
-          {children}
+        <div className="w-full h-full flex flex-col gap-y-3">
+          <Projects />
+          <div className="flex flex-row gap-x-4">
+            <Sortbutton />
+            <View />
+          </div>
+          <div className="w-full h-full flex flex-row">{children}</div>
         </div>
       </div>
     </div>
