fe89fbb6c884df53883f22858e74700f4dd7e647
amanprajapati00 <aman.prajapati3000@gmail.com>
2024-11-06 23:07:04 +0530

    fixed bugs

--- lib/auth/auth.config.ts
+++ lib/auth/auth.config.ts
@@ -2,7 +2,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import { NextAuthConfig } from "next-auth";
 import Google from "next-auth/providers/google";
-import bcrypt from 'bcryptjs';
+import { compare } from 'bcryptjs';
 import Credentials from "next-auth/providers/credentials";
 const prisma = new PrismaClient();
 export const authOptions = {
@@ -43,7 +43,7 @@ export const authOptions = {
         if (
           existingUser &&
           existingUser.password &&
-          (await bcrypt.compare(credentials.password, existingUser.password))
+          (await compare(credentials.password, existingUser.password))
         ) {
           console.log("signed in, user found with correct credentials");
           return {
