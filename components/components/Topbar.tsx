9631a8bf63712d5370591c1162f9e7f352b5bb60
amanprajapati00 <aman.prajapati3000@gmail.com>
2024-11-06 23:23:11 +0530

    added searchbar

--- components/components/Topbar.tsx
+++ components/components/Topbar.tsx
@@ -1,17 +1,14 @@
 "use client";
 import React from "react";
-import { Button } from "../ui/button";
 import Info from "./Info";
 import { Plus } from "lucide-react";
+import Searchbar from "./Searchbar";
 
 export default function Topbar() {
   return (
     <div className="w-full h-max rounded-lg bg-[#3274a9] mb-3">
       <div className="flex flex-row justify-between items-center p-3">
-        <div className="flex flex-row items-center py-2 px-3 border-2 border-[#0a0f29] gap-x-4 rounded-lg">
-          <Plus className="bg-[#1a287e] size-10 rounded-lg p-1 text-white" />
-          <span className="text-xl text-white">Add Project</span>
-        </div>
+        <Searchbar />
         <Info />
       </div>
     </div>
