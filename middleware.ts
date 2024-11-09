4f9ad48c1fa49918ff50f2ab66c2d4c06c615647
amanprajapati00 <aman.prajapati3000@gmail.com>
2024-11-09 18:52:09 +0530

    bugs

--- middleware.ts
+++ middleware.ts
@@ -1,69 +1,61 @@
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
-import NextAuth from "next-auth";
 import {
+  publicRoutes,
   authPrefix,
   authRoutes,
   DEFAULT_LOGIN_REDIRECT,
-  publicRoutes,
-} from "./lib/routes";
-import { authOptions } from "./lib/auth/auth.config";
+} from "@/lib/routes";
 
-const { auth } = NextAuth(authOptions);
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const isLoggedIn = !!token;
 
-export default auth(async function middleware(req: NextRequest) {
-  try {
-    const token = await getToken({ req, secret:process.env.NEXTAUTH_SECRET });
-    const isLoggedIn = !!token;
-    const path = req.nextUrl.pathname;
-    const isAuthApiRoute = path.startsWith(authPrefix);
-    const isPublicRoute = publicRoutes.includes(path);
-    const isApiRoute = authRoutes.includes(path);
+  const path = req.nextUrl.pathname;
+  const isApiAuthRoute = path.startsWith(authPrefix);
+  const isPublicRoute = publicRoutes.includes(path);
+  const isAuthRoute = authRoutes.includes(path);
 
-    console.log({
-      path,
-      isLoggedIn,
-      isApiRoute,
-      isAuthApiRoute,
-      isPublicRoute,
-      timeStamp: new Date().toISOString(),
-    });
+  console.log({
+    path,
+    isLoggedIn,
+    isApiAuthRoute,
+    isPublicRoute,
+    isAuthRoute,
+    timestamp: new Date().toISOString(),
+  });
 
-    if (isAuthApiRoute) {
-      return NextResponse.next();
-    }
+  if (isApiAuthRoute) {
+    return null;
+  }
 
-    if (isApiRoute) {
-      if (isLoggedIn) {
-        console.log(
-          `Redirecting logged-in user from ${path} to ${DEFAULT_LOGIN_REDIRECT}`
-        );
-        return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, req.url));
-      }
-      return NextResponse.next();
-    }
-
-    if (!isLoggedIn && !isPublicRoute) {
-      console.log(`Redirecting non-logged-in user from ${path} to signin`);
-      const signInURL = new URL("/signin", req.url);
-      signInURL.searchParams.set("callbackUrl", req.url);
-      return NextResponse.redirect(signInURL);
-    }
-
-    if (isLoggedIn && isPublicRoute && path !== DEFAULT_LOGIN_REDIRECT) {
+  if (isAuthRoute) {
+    if (isLoggedIn) {
       console.log(
         `Redirecting logged-in user from ${path} to ${DEFAULT_LOGIN_REDIRECT}`
       );
       return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, req.url));
     }
-
-    return NextResponse.next();
-  } catch (e) {
-    console.error(e);
-    return NextResponse.error();
+    return null;
   }
-});
+
+  if (!isLoggedIn && !isPublicRoute) {
+    console.log(`Redirecting non-logged-in user from ${path} to signin`);
+    const signInUrl = new URL("/signin", req.url);
+    signInUrl.searchParams.set("callbackUrl", req.url);
+    return NextResponse.redirect(signInUrl);
+  }
+
+  if (isLoggedIn && isPublicRoute && path !== DEFAULT_LOGIN_REDIRECT) {
+    console.log(
+      `Redirecting logged-in user from public route ${path} to ${DEFAULT_LOGIN_REDIRECT}`
+    );
+    return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, req.url));
+  }
+
+  return null;
+}
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api)(.*)"],
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
