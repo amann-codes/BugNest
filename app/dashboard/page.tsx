e89deca4ae4bd623cc47f515a0e343717fc8eae6
amanprajapati00 <aman.prajapati3000@gmail.com>
2024-11-09 19:00:53 +0530

    changed layout and page for dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,25 @@
+"use client";
 import Sortbutton from "@/components/components/Sortbutton";
 import IssueScreen from "@/components/components/IssueScreen";
-import React from "react";
+import React, { useState } from "react";
 import Searchbar from "@/components/components/Searchbar";
 import View from "@/components/components/View";
+import Projects from "@/components/components/Projects";
+import Topbar from "@/components/components/Topbar";
 
 export default function page() {
+  const [selectProjectId, setSelectProjectId] = useState<string>("");
+
+  const handleSelectProject = (projectId: string) => {
+    setSelectProjectId(projectId);
+  };
   return (
-    <div className="w-max h-full flex flex-col gap-y-3">
-      <div className="flex flex-row justify-between"></div>
-      <IssueScreen />
+    <div className="flex flex-col p-3">
+      <Topbar />
+      <div className="flex flex-row gap-x-3">
+        <Projects onSelectProject={handleSelectProject} />
+        <IssueScreen projectId={selectProjectId} />
+      </div>
     </div>
   );
 }
